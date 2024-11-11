9bf9b2f58682948dd30b7606c6610d4d08f52a11
Suraj Chaudhary <99071327+surajcdry@users.noreply.github.com>
2024-11-11 12:20:37 -0500

    Update tictactoe.py
    Added heading of the game

--- tictactoe.py
+++ tictactoe.py
@@ -13,6 +13,8 @@ while play:
     gameRunning = True
 
     def gameboard(board):
+        print("Tic Tac Toe")
+        print()
         print(board[0] + " | " + board[1] + " | " + board[2])
         print(board[3] + " | " + board[4] + " | " + board[5])
         print(board[6] + " | " + board[7] + " | " + board[8])
